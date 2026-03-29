fe23c35a6ee25ac5baa6a61b9d2f5b6cad5703fc
yeatfish <yucyuy@gmail.com>
2026-03-29 19:44:02 +0800

    加入markdown

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.CORS_ORIGIN?.split(',') ?? ['http://localhost:5173', 'http://localhost:5174'],
+    origin: process.env.CORS_ORIGIN?.split(',') ?? ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
     credentials: true,
   });
 
